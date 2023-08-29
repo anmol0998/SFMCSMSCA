01ffb25641ad65e48777e37d452ec85dc7519888
anmol0998 <104828995+anmol0998@users.noreply.github.com>
2023-08-29 18:16:21 +0530

    updated

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.TwilioV1.TwilioNumber}}" //<----This should map to your data extension name and phone number column
+            "to": "{{{{Event.DEAudience-cff90d00-64cc-0ed5-4af8-cf6454267a81.TwilioNumber}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
